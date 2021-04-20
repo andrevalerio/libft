e4acfab9430e4ba0d8656cce693726697c5d41ff
andrevalerio <avavah@gmail.com>
2021-04-19 22:33:34 -0300

    norminette v3 adjusted

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: avalerio <avalerio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 22:34:03 by avalerio          #+#    #+#             */
-/*   Updated: 2021/02/14 22:41:47 by avalerio         ###   ########.fr       */
+/*   Updated: 2021/04/19 22:32:41 by avalerio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *element;
+	t_list	*element;
 
 	if (!lst)
 		return ;
